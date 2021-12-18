23f7cced4115c90c5aa278f00b3fd94ecca1b306
Gergely Brautigam <182850+Skarlso@users.noreply.github.com>
2021-12-18 11:20:31 +0100

    Quick and dirty

--- day16/part2/main.go
+++ day16/part2/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+	"math"
 	"os"
 	"strconv"
 )
@@ -21,21 +22,13 @@ func main() {
 		binary += b
 	}
 
-	parseBits(binary, 0, 0)
-	fmt.Println("sum: ", sum)
+	v, _ := parseBits(binary, 0, 0)
+	fmt.Println("value: ", v)
 }
-
-var sum int
 
 // Does the parsing. Gets an index and the depth of the current package.
 // Returns the accumulated version number.
-func parseBits(binary string, i, depth int) int {
-	version := binary[i : i+3]
-	output, err := strconv.ParseInt(version, 2, 64)
-	if err != nil {
-		log.Fatal(err)
-	}
-	sum += int(output)
+func parseBits(binary string, i, depth int) (int, int) {
 	typeID := binary[i+3 : i+6]
 	t, err := strconv.ParseInt(typeID, 2, 64)
 	if err != nil {
@@ -44,14 +37,18 @@ func parseBits(binary string, i, depth int) int {
 	// parse literal value
 	if t == 4 {
 		i += 6
+		value := 0
 		for {
+			n, _ := strconv.ParseInt(binary[i+1:i+5], 2, 64)
+			value = value*16 + int(n)
 			i += 5
 			if binary[i-5] == '0' {
-				return i
+				return value, i
 			}
 		}
 	} else {
 		ltid := binary[i+6]
+		values := make([]int, 0)
 		// 15 bits
 		if ltid == '0' {
 			lenBits, err := strconv.ParseInt(binary[i+7:i+7+15], 2, 64)
@@ -61,7 +58,8 @@ func parseBits(binary string, i, depth int) int {
 			startIndex := i + 7 + 15
 			i = startIndex
 			for {
-				nextIndex := parseBits(binary, i, depth)
+				val, nextIndex := parseBits(binary, i, depth)
+				values = append(values, val)
 				i = nextIndex
 				if nextIndex-startIndex == int(lenBits) {
 					break
@@ -75,11 +73,64 @@ func parseBits(binary string, i, depth int) int {
 			}
 			i += (7 + 11)
 			for n := 0; n < int(numberOfPackets); n++ {
-				nextIndex := parseBits(binary, i, depth)
+				val, nextIndex := parseBits(binary, i, depth)
+				values = append(values, val)
 				i = nextIndex
+			}
+		}
+		switch t {
+		case 0:
+			return sum(values), i
+		case 1:
+			s := 1
+			for _, v := range values {
+				s *= v
+			}
+			return s, i
+		case 2:
+			min := math.MaxInt
+			for _, v := range values {
+				if v < min {
+					min = v
+				}
+			}
+			return min, i
+		case 3:
+			max := 0
+			for _, v := range values {
+				if v > max {
+					max = v
+				}
+			}
+			return max, i
+		case 5:
+			if values[0] > values[1] {
+				return 1, i
+			} else {
+				return 0, i
+			}
+		case 6:
+			if values[0] < values[1] {
+				return 1, i
+			} else {
+				return 0, i
+			}
+		case 7:
+			if values[0] == values[1] {
+				return 1, i
+			} else {
+				return 0, i
 			}
 		}
 	}
 
-	return i
+	return 0, i
+}
+
+func sum(values []int) int {
+	sum := 0
+	for _, v := range values {
+		sum += v
+	}
+	return sum
 }
